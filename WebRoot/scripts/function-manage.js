b49bd8ee0dc2a38b4862c812bbc30cc2b1b93f41
mtl <1124539578@qq.com>
2018-01-11 21:50:06 +0800

    201801112150

--- WebRoot/scripts/function-manage.js
+++ WebRoot/scripts/function-manage.js
@@ -2,14 +2,15 @@
 function Delete(deleteType,id)
 {
 	if(confirm("确定要删除吗？")) {
-		
-		
 		$.ajax({
 			url:"deleteServlet?"+deleteType+"&"+id,
 			type:"post",	
 			success:function(result){
 				if(result>0){
 					alert("您要删除的存在关联数据，无法删除");
+					return false;
+				}else{
+					location.href = "deleteServlet?"+deleteType+"&"+id;
 				}
 			}
 		});
